d9f21134fbc26e58c3964c6af07b4c89e3623736
Thimee <timswaggs1@outlook.com>
2022-08-23 19:16:31 -0700

    submission

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-first_string = "Last digit of"
-last_digit = number % 10
-if last_digit > 5:
-	rem = "and is greater than 5"
-elif last_digit == 0:
-	rem = "and is 0"
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-	rem = "and is less than 6 and not 0"
-print(f"{first_string} {number:d} is {last_digit:d} {rem}")
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
-exit
-exit()
+    if n >= 0:
+        return str[:n] + str[n+1:]
+    else:
+        return str
