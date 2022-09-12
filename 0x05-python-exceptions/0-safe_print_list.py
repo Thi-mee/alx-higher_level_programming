5316a685806c0c291972acf8589032b773f9bc9d
Thimee <timswaggs1@outlook.com>
2022-09-12 16:19:09 -0700

    initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+
+    for item in my_list:
+        if i < x:
+            try:
+                print("{}".format(item), end='')
+                i += 1
+            except Error:
+                break
+    return (i)
