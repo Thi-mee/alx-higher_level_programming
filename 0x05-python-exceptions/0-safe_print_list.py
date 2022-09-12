c983ca406b9c62635c041d187cc78dae2a484bea
Thimee <timswaggs1@outlook.com>
2022-09-12 16:25:13 -0700

    fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print("{}".format(item), end='')
                 i += 1
-            except Error:
+            except IndexError:
                 break
     return (i)
