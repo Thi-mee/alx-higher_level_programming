43e80e5d9c6ae125dadf68badf99a095dc8190a2
Timi Adenuga <timswaggs1@outlook.com>
2022-11-29 05:03:53 +0100

    tasks for today

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const occurences = parseInt(process.argv[2],10);
+const occurences = parseInt(process.argv[2], 10);
 
-if (isNaN(occurences)){
+if (isNaN(occurences)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < occurences; i++){
+  for (let i = 0; i < occurences; i++) {
     console.log('C is fun');
   }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let size = parseInt(process.argv[2], 10);
-if (isNaN(size)){
+const size = parseInt(process.argv[2], 10);
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
