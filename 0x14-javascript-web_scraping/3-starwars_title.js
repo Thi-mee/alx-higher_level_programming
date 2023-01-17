3863d9a1f68fd0ab98d7abdcbef7eeac522fd56c
Timi Adenuga <timswaggs1@outlook.com>
2023-01-18 00:04:05 +0100

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-const epId = process.argv[2];
-const apiUrl = `http://swapi.co/api/films/${epId}`;
-request.get(apiUrl, (error, response, body) => {
-  if (error) console.log(error);
-  console.log(JSON.parse(body).title);
+const MovieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId;
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = process.argv[2];
 
-request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let countWedge = 0;
-    const films = JSON.parse(body).results;
-    for (let i = 0; i < films.length; i++) {
-      const charList = films[i].characters;
-      for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
-          countWedge += 1;
-        }
-      }
-    }
-    console.log(countWedge);
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (err, response, body) {
+  let count = 0;
+  if (err) {
+    console.log(err);
   }
+  const data = JSON.parse(body);
+  for (let i = 0; data.results[i] !== undefined; i++) {
+    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
 });
